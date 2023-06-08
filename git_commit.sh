bdaa65019f9ae26e8f49ec54b063d898aef3b213
Matt Hauenstein <101595028+mhauenst@users.noreply.github.com>
2023-06-08 12:49:35 -0400

    Update automatic commit date stamp

--- git_commit.sh
+++ git_commit.sh
@@ -2,7 +2,7 @@
 cd C:/Users/mhauenst/Documents/sbeconomydb/
 git add --all
 timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
+  date +"on %m/%d/%Y"
 }
 git commit -am "Auto-commit data updates $(timestamp)"
 git push origin main
