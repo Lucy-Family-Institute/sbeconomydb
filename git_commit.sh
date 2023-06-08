406cec1d19bb01a190a19f817d89c08e9525a414
Matt Hauenstein <101595028+mhauenst@users.noreply.github.com>
2023-06-08 12:38:39 -0400

    Auto-commit data updates at 12:38:39 on 08/06/2023

---
+++ git_commit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+cd C:/Users/mhauenst/Documents/sbeconomydb/
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Auto-commit data updates $(timestamp)"
+git push origin master
